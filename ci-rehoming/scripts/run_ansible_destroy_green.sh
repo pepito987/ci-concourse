8d2d3600ddf1a37d5d10f8eb826de115768b378a
Giuseppe <giuseppe.quartarone@digital.hmrc.gov.uk>
2017-04-22 18:02:48 +0100

    Add outputs for combined output

--- ci-rehoming/scripts/run_ansible_destroy_green.sh
+++ ci-rehoming/scripts/run_ansible_destroy_green.sh
@@ -3,3 +3,7 @@
 echo "run ansible green test"
 
 ansible-playbook ansible/playbooks/hello_world.yaml
+
+echo "destroy done" >> out/out.txt
+
+cat out/out.txt
--- ci-rehoming/scripts/run_ansible_green.sh
+++ ci-rehoming/scripts/run_ansible_green.sh
@@ -3,3 +3,7 @@
 echo "run ansible green test"
 
 ansible-playbook ansible/playbooks/hello_world.yaml
+
+echo "ansible green done" >> out/out.txt
+
+cat out/out.txt
