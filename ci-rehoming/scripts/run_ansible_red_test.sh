af040030171335b88dc9864fb8700bf76ae7ae44
Giuseppe <giuseppe.quartarone@digital.hmrc.gov.uk>
2017-04-25 23:01:13 +0100

    Fix script

--- ci-rehoming/scripts/run_ansible_red_test.sh
+++ ci-rehoming/scripts/run_ansible_red_test.sh
@@ -2,6 +2,6 @@
 
 echo "run ansible failing test"
 
+cp ci-concourse/ci-rehoming/scripts/message/message.txt out
 ansible-playbook ansible/playbooks/fail.yaml
 
-cp ci-concourse/ci-rehoming/scripts/message/message.txt out
